71b6d579129549c9981a86e32f38eb3a15580d08
Glaydson Vasconcelos de Sousa <glaydsonvs@gmail.com>
2019-05-16 15:05:56 -0300

    Métodos básicos de AutorController

--- livraria-api/src/main/java/br/edu/unichristus/controladores/AutorController.java
+++ livraria-api/src/main/java/br/edu/unichristus/controladores/AutorController.java
@@ -4,16 +4,20 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.unichristus.entidades.Autor;
+import br.edu.unichristus.excecoes.AutorNaoEncontradoException;
 import br.edu.unichristus.servicos.AutorService;
 
 @RestController
@@ -33,6 +37,12 @@ public class AutorController {
 		return servicoAutores.buscarPeloID(id);
 	}
 	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteAutor(@PathVariable("id") Long id) {
+		servicoAutores.remover(id);
+		return new ResponseEntity<>("Autor foi removido!", HttpStatus.OK);
+	}
+	
 	@PostMapping("/novo")
 	public ResponseEntity<Object> salvar(@RequestBody Autor autor) {
 		Autor autorSalvo = servicoAutores.salvar(autor);
@@ -40,5 +50,18 @@ public class AutorController {
 				.buildAndExpand(autorSalvo.getAutorID()).toUri();
 		return ResponseEntity.created(local).build();
 	}
+	
+	@PutMapping("/atualiza/{id}")
+	public ResponseEntity<Object> atualizaAutor(@RequestBody Autor autor, @PathVariable long id) {
+		try {
+			Autor autorBD = servicoAutores.buscarPeloID(id);
+		} catch (AutorNaoEncontradoException lnee) {
+			return ResponseEntity.notFound().build();
+		}
+		autor.setAutorID(id);	
+		servicoAutores.salvar(autor);
+		return ResponseEntity.noContent().build();
+	}
+
 
 }
